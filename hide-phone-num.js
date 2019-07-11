b719e6224c41cbd0d38c1d6f46e272abe5ce7922
Sergey Korn <crealime@gmail.com>
2019-07-11 13:45:47 +0300

    Update hide-phone-num.js

--- hide-phone-num.js
+++ hide-phone-num.js
@@ -1,24 +1,24 @@
 (function($) {
-		$.fn.hidePhoneNum = function(options) {
+	$.fn.hidePhoneNum = function(options) {
 
-			var settings = $.extend({
-				showNumber: 3,
-				linkClass: 'tel-link'
-			}, options);
+		var settings = $.extend({
+			showNumber: 3,
+			linkClass: 'tel-link'
+		}, options);
 
-			return this.each(function() {
-				var telNum = $(this).data("tel"),
-						htmlNum = $(this).html(),
-						myRegex = /[\s-\(\()]/g,
-						telNewNum = telNum.replace(myRegex, '');
+		return this.each(function() {
+			var telNum = $(this).data("tel"),
+					htmlNum = $(this).html(),
+					myRegex = /[\s-\(\()]/g,
+					telNewNum = telNum.replace(myRegex, '');
 
-				$(this).html(telNum.substr(0, settings.showNumber) + " " + htmlNum);
+			$(this).html(telNum.substr(0, settings.showNumber) + " " + htmlNum);
 
-				$(this).find('a').on('click', function(e) {
-					e.preventDefault();
-					$(this).parent().html("<a href='tel:"+telNewNum+"' class='"+settings.linkClass+"'>"+telNum+"</a>")
-				})
+			$(this).find('a').on('click', function(e) {
+				e.preventDefault();
+				$(this).parent().html("<a href='tel:"+telNewNum+"' class='"+settings.linkClass+"'>"+telNum+"</a>")
+			})
 
-			});
-		};
-	}(jQuery));
+		});
+	};
+}(jQuery));
