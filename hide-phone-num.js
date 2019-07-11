019a78329fb743dee9bcc2e90ae9cd917a8b4ac7
Sergey Korn <crealime@gmail.com>
2019-07-11 13:35:43 +0300

    Create hide-phone-num.js

---
+++ hide-phone-num.js
@@ -0,0 +1,24 @@
+(function($) {
+		$.fn.hidePhoneNum = function(options) {
+
+			var settings = $.extend({
+				showNumber: 3,
+				linkClass: 'tel-link'
+			}, options);
+
+			return this.each(function() {
+				var telNum = $(this).data("tel"),
+						htmlNum = $(this).html(),
+						myRegex = /[\s-\(\()]/g,
+						telNewNum = telNum.replace(myRegex, '');
+
+				$(this).html(telNum.substr(0, settings.showNumber) + " " + htmlNum);
+
+				$(this).find('a').on('click', function(e) {
+					e.preventDefault();
+					$(this).parent().html("<a href='tel:"+telNewNum+"' class='"+settings.linkClass+"'>"+telNum+"</a>")
+				})
+
+			});
+		};
+	}(jQuery));
